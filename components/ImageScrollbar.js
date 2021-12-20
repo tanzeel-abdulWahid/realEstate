2a19409b5487eda5d34682e81765495c3a42b94c
tanzeel <tanzeelwahid111@gmail.com>
2021-12-20 14:09:02 +0500

    deploying

--- components/ImageScrollbar.js
+++ components/ImageScrollbar.js
@@ -4,7 +4,7 @@ import { Box, Icon, Flex } from '@chakra-ui/react';
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa';
 
-const leftArrow = () => {
+const LeftArrow = () => {
     const {scrollPrev} = useContext(VisibilityContext);
 
     return(
@@ -18,7 +18,7 @@ const leftArrow = () => {
         </Flex>
     );
 }
-const rightArrow = () => {
+const RightArrow = () => {
     const {scrollNext} = useContext(VisibilityContext);
 
     return(
@@ -36,9 +36,9 @@ const rightArrow = () => {
 
 const ImageScrollbar = ({data}) => {
     return (
-        <ScrollMenu LeftArrow={leftArrow} RightArrow={rightArrow} style={{overflow: 'hidden'}}>
+        <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow} style={{overflow: 'hidden'}}>
             {data.map((item) => (
-                <Box width='910px' itemId={item.id} overflow='hidden' p={'1'}>
+                <Box width='910px' key={item.id} itemId={item.id} overflow='hidden' p={'1'}>
                     <Image placeholder="blur" blurDataURL={item.url} src={item.url}
                     width={1000} height={500} sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"/>
                 </Box>
